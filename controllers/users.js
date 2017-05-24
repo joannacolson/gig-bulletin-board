65dfc3745b9e711dd25ff81d879821d6718eec48
jadehartman <rachel.jade.hartman@gmail.com>
2017-05-24 16:28:18 -0700

    update usersctrl

--- controllers/users.js
+++ controllers/users.js
@@ -8,6 +8,7 @@ router.route('/')
         User.find(function(err, users) {
             if (err) return res.status(500).send(err);
 
+            console.log("Find all users data:", users);
             return res.send(users);
         });
     });
